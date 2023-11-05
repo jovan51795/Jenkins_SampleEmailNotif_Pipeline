2b4afd3416168d4559a8abf81fe9805466bbf93b
jovan51795 <62101107+jovan51795@users.noreply.github.com>
2023-11-05 13:10:55 +0800

    remove the error

--- src/main/java/com/jovscb/CalculatorService.java
+++ src/main/java/com/jovscb/CalculatorService.java
@@ -4,6 +4,6 @@ public class CalculatorService {
     public  int add(int a, int b) {
 
         System.out.println("this is sample class");
-        return a + b + a;
+        return a + b;
     }
 }
