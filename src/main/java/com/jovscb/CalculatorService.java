868dba8b81dc6415013aa54aa830d9c62e52a586
jovan51795 <62101107+jovan51795@users.noreply.github.com>
2023-11-05 13:08:24 +0800

    add error

--- src/main/java/com/jovscb/CalculatorService.java
+++ src/main/java/com/jovscb/CalculatorService.java
@@ -4,6 +4,6 @@ public class CalculatorService {
     public  int add(int a, int b) {
 
         System.out.println("this is sample class");
-        return a + b;
+        return a + b + a;
     }
 }
